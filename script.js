a58f9dbfe57d36efbd7b0ed0b2cf22b98ac41d76
Do Khac Duong <90135701+dokhacduong1@users.noreply.github.com>
2023-08-24 03:24:46 +0700

    first commit6

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 
-
-
-
-
+import {submitShin} from "./shin.js"
 const shinSubmit = document.querySelector(".shin-submit")
 shinSubmit.addEventListener("click",(e)=>{
     e.preventDefault()
     const test =submitShin(e)
     console.log(test)
 })
-
-
 
 
 
--- shin.js
+++ shin.js
@@ -8,15 +8,17 @@ shinFormItem.forEach(ItemFor => {
     const getTextArea = ItemFor.querySelector("textarea")
     //Lấy dữ lệu thuộc tính của name trong shin-form-item
     const nameItem = ItemFor.getAttribute("name")
-     //Lấy dữ lệu thuộc tính của label trong shin-form-item
+
+    //Lấy dữ lệu thuộc tính của label trong shin-form-item
     const labelItem = ItemFor.getAttribute("label")
     //Đoạn này lấy input hoặc textarea nếu khác null ưu tiên sẽ lấy input 
     const getTextAll = (getInput != null) ? getInput : getTextArea
 
     //Kiểm tra nếu đủ thành phần sẽ vào trong if
-    if (getTextAll != null && nameItem != null && labelItem != null) {
+    if (getTextAll != null && nameItem != null && labelItem != null && nameItem != null && labelItem != null) {
         //set cho cái input hoặc textarea bằng cái nameItem để định nghĩa id của nó
         getTextAll.setAttribute("id", nameItem)
+        getTextAll.setAttribute("name", nameItem)
         //Tạo một lable mới
         const labelNew = document.createElement("label")
         //set cho cái label bằng cái nameItem để định nghĩa for của nó
@@ -30,28 +32,41 @@ shinFormItem.forEach(ItemFor => {
 })
 
 
- /**
-     * @param e Sự Kiện Khi Ấn Submit Trong Form
- */
-function submitShin (e){
-    function handleSubmitShin(){
+/**
+    * @param e Sự Kiện Khi Ấn Submit Trong Form
+*/
+function submitShin(e) {
+    function handleSubmitShin() {
         const shinForm = e.target.parentNode
         const shinFormItem = shinForm.querySelectorAll(".shin-form-item")
         const formData = {};
-    
+        
         shinFormItem.forEach(item => {
             const input = item.querySelector('input, textarea');
             if (input) {
                 const name = input.parentNode.getAttribute('name');
-                const value = input.value;
-                formData[name] = value;
+                const checkType = input.getAttribute("type")
+                
+                if(checkType === "radio" || checkType === "checkbox"){         
+                    if(input.checked){
+                        const value = input.value;         
+                        formData[name] = value;
+                    }
+                }else{
+                    if (name !== null) {
+                        const value = input.value;
+                        formData[name] = value;
+                    }
+                }
+               
+
             }
         });
-    
+
         return formData
     }
-   return handleSubmitShin()
-   
+    return handleSubmitShin()
+
 }
 //---------------------------------------------------------------------------------------------------------
 
