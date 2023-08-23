e532a34776d807b78774e6704e19871d5eca17f2
Do Khac Duong <90135701+dokhacduong1@users.noreply.github.com>
2023-08-24 03:42:01 +0700

    first commit7

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
 
-import {submitShin} from "./shin.js"
-const shinSubmit = document.querySelector(".shin-submit")
-shinSubmit.addEventListener("click",(e)=>{
-    e.preventDefault()
-    const test =submitShin(e)
-    console.log(test)
-})
 
 
+function submitShin(e) {
+    function handleSubmitShin() {
+        const shinForm = e.target
+        const shinFormItem = shinForm.querySelectorAll(".shin-form-item")
+        const formData = {};
+        
+        shinFormItem.forEach(item => {
+           
+            const input = item.querySelector('input, textarea');
+            if (input) {
+                const name = input.parentNode.getAttribute('name');
+                const checkType = input.getAttribute("type")
+                
+                if(checkType === "radio" || checkType === "checkbox"){         
+                    if(input.checked){
+                        const value = input.value;         
+                        formData[name] = value;
+                    }
+                }else{
+                    if (name !== null) {
+                        const value = input.value;
+                        formData[name] = value;
+                    }
+                }
+               
 
+            }
+        });
 
+        return formData
+    }
+    return handleSubmitShin()
+
+}
+const handleSubmit = (e)=>{
+    e.preventDefault();
+    const dataForm = submitShin(e)
+    console.log(dataForm)
+}
+
+const handleSubmit1 = (e)=>{
+    e.preventDefault();
+    const dataForm = submitShin(e)
+    console.log(dataForm)
+}
+const handleSubmit2 = (e)=>{
+    e.preventDefault();
+    const dataForm = submitShin(e)
+    console.log(dataForm)
+}
 
--- shin.js
+++ shin.js
@@ -37,28 +37,28 @@ shinFormItem.forEach(ItemFor => {
 */
 function submitShin(e) {
     function handleSubmitShin() {
-        const shinForm = e.target.parentNode
+        const shinForm = e.target
         const shinFormItem = shinForm.querySelectorAll(".shin-form-item")
         const formData = {};
-        
+
         shinFormItem.forEach(item => {
             const input = item.querySelector('input, textarea');
             if (input) {
                 const name = input.parentNode.getAttribute('name');
                 const checkType = input.getAttribute("type")
-                
-                if(checkType === "radio" || checkType === "checkbox"){         
-                    if(input.checked){
-                        const value = input.value;         
+
+                if (checkType === "radio" || checkType === "checkbox") {
+                    if (input.checked) {
+                        const value = input.value;
                         formData[name] = value;
                     }
-                }else{
+                } else {
                     if (name !== null) {
                         const value = input.value;
                         formData[name] = value;
                     }
                 }
-               
+
 
             }
         });
