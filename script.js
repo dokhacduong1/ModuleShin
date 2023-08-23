4c9fa67dd3fd00b9e9c222bcba34a036f9026e6f
Do Khac Duong <90135701+dokhacduong1@users.noreply.github.com>
2023-08-24 03:43:56 +0700

    first commit8

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
 
-
-
-function submitShin(e) {
-    function handleSubmitShin() {
-        const shinForm = e.target
-        const shinFormItem = shinForm.querySelectorAll(".shin-form-item")
-        const formData = {};
-        
-        shinFormItem.forEach(item => {
-           
-            const input = item.querySelector('input, textarea');
-            if (input) {
-                const name = input.parentNode.getAttribute('name');
-                const checkType = input.getAttribute("type")
-                
-                if(checkType === "radio" || checkType === "checkbox"){         
-                    if(input.checked){
-                        const value = input.value;         
-                        formData[name] = value;
-                    }
-                }else{
-                    if (name !== null) {
-                        const value = input.value;
-                        formData[name] = value;
-                    }
-                }
-               
-
-            }
-        });
-
-        return formData
-    }
-    return handleSubmitShin()
-
-}
 const handleSubmit = (e)=>{
     e.preventDefault();
     const dataForm = submitShin(e)
